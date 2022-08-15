7f553b2bab2276ee06f76d8a043924fabd1b6b5a
Mamba24 <akateason@qq.com>
2022-08-16 01:15:37 +0800

    [annotation] 1

--- XTUtil-swift/Source/XTUtil.swift
+++ XTUtil-swift/Source/XTUtil.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 public class XTUtil: NSObject {
+    
+    /// 测量方法的时间
     public static func measure(f: ()->()) {
         let start = CACurrentMediaTime()
         f()
@@ -16,6 +18,8 @@ public class XTUtil: NSObject {
     }
 }
 
+
+/// print
 public func xtPrint<T>(_ message: T, fileName: String = #file, funcName: String = #function, lineNum : Int = #line) {
     #if DEBUG
         print("「 \((fileName as NSString).lastPathComponent) 第\(lineNum)行 \(funcName) 」\("\n")\(message)")
